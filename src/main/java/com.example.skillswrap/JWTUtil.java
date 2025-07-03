43cd7528fa54c7dca295101956a3c1004cf2d933
Ryan Pugh <74481946+rpugh10@users.noreply.github.com>
2025-07-02 23:22:52 -0700

    Add files via upload

--- src/main/java/com.example.skillswrap/JWTUtil.java
+++ src/main/java/com.example.skillswrap/JWTUtil.java
@@ -1,1 +1,46 @@
+package com.example.skillswap.configuration;
 
+import com.example.skillswap.entity.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.*;
+
+import java.util.Date;
+
+@Component
+public class JWTUtil {
+    @Value("${app.jwp.secret}")
+    private String jwtSecret;
+
+    @Value(("${app.jwt.expiration}"))
+    private Long jwtExpiration;
+
+    public String generateToken(User user)
+    {
+        return Jwts.builder()
+                .setSubject(user.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public String extractUsername(String token)
+    {
+        return Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean isTokenValid(String token)
+    {
+        try{
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            return true;
+        }catch(JwtException e){
+            return false;
+        }
+    }
+}
